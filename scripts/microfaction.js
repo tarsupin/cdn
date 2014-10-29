bdaaf8ad1e556d82e848d515db831af004a83898
Jon Leckie <jonleckie@yahoo.com>
2014-10-29 12:44:03 -0500

    Adding function to microfaction.js to load rules via AJAX call

--- scripts/microfaction.js
+++ scripts/microfaction.js
@@ -78,3 +78,31 @@ function minimize(id)
 		minBox.innerHTML = "Maximize";
 	}
 }
+
+/***************************
+****** Posting Scripts ******
+***************************/
+
+function showRules(url, hashtag)
+{
+	// Create the parser that will work on our current URL
+    var parser = document.createElement('a'),
+        searchObject = {},
+        queries, split, i;
+    // Let the browser do the work
+    parser.href = url;
+    // Convert query string to object
+    queries = parser.search.replace(/^\?/, '').split('&');
+    for( i = 0; i < queries.length; i++ ) {
+        split = queries[i].split('=');
+        searchObject[split[0]] = split[1];
+    }
+    // parser.hostname will return _____.microfaction.test
+    // This next line will typecast our entire parser.hostname as a String and
+    // return the portion of it beginning at position 0 and ending at the first period(.)
+    // (In other words, the "_____" portion of parser.hostname)
+    var microfaction = String(parser.hostname).substr(0, String(parser.hostname).indexOf('.'));
+    // Now that we have the name of the microfaction that we are in, we can pass that 
+    // to our AJAX function to tell it where the rules can be found
+	loadAjax("", "load-rules", "rules", "microfaction=" + microfaction, "hashtag=" + hashtag);
+}
